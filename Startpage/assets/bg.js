815d67a7383665de2ecdf01b4e0b22dfa6bcdc6e
Gaspar Octavio <gasparcorreavergara@gmail.com>
2017-03-12 17:36:59 -0300

    startpage updated

---
+++ Startpage/assets/bg.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function onHover()
+{
+    selected_background = getRandomInt(1, 48);
+    document.getElementById("backgroundf").src = "bg/x.jpg".replace("x",selected_background);
+}
+
+function offHover()
+{
+    selected_background = getRandomInt(1, 48);
+    document.getElementById("backgroundb").src = "bg/x.jpg".replace("x",selected_background);
+}
+
+selected_background = getRandomInt(1, 48)
+document.getElementById("backgroundf").src = "bg/x.jpg".replace("x",selected_background)
